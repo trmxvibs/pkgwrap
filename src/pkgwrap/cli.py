e2456f064aa154826a9a564789bbb70a6465879f
Lokesh Kumar <termuxvibes@gmail.com>
2026-08-18 18:17:27 +0530

    fix: remove duplicate import introduced by the merge

--- src/pkgwrap/cli.py
+++ src/pkgwrap/cli.py
@@ -10,8 +10,6 @@ import argparse
 import os
 import sys
 from typing import List, Optional, Sequence
-
-from pkgwrap.package_map import resolve_package_name
 from pkgwrap import __version__
 from pkgwrap.backends import available_backends, get_backend
 from pkgwrap.config import clear_cache, get_cache_file
